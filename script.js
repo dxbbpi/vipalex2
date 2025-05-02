db12b623dbfa42b4a0003b62b7ff380c2ef65886
dxbbpi <olafz9zzz@gmail.com>
2025-05-02 22:43:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ document.getElementById("vipForm").addEventListener("submit", async function (e)
     };
 
     try {
-      const res = await fetch("https://dxbbpi.github.io/vipalex2/upload", {
+      const res = await fetch("https://vipalex.onrender.com/upload", {
         method: "POST",
         headers: {
           "Content-Type": "application/json", // ใช้ JSON แทน FormData
